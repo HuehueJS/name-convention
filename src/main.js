ba89deca10dc19731274d4b80248dabac1fce934
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-04-24 12:54:34 -0300

    setup project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+export class Test{
+
+}
+
+function huehue(){
+    let a,b;
+    [a,,b] = [1,2,3]
+    return [a,b]
+}
+
+module.exports = huehue
---
+++ test/main.js
@@ -0,0 +1,9 @@
+const huehue = require('../src/main');
+const expect = require('chai').expect;
+
+
+describe('#huehue',function(){
+    it('',function(){
+        expect(huehue()).to.deep.equal([1,3])
+    })
+})
