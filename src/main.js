7cdd5f354177274dca1420c96ba601dd0a6ca3c9
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-04-27 12:32:15 -0300

    getattr++

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-const global_functions = {
-
-    getattr : function(obj,key,value=undefined){
-        return key in obj ? obj[key] : value;
+export const getattr = function (obj, key, defaultValue = undefined) {
+    if (obj === undefined || obj === null) {
+        return defaultValue;
     }
-
+    if( typeof obj !== 'object'){
+        throw TypeError();
+    }
+    if (['string','number'].indexOf(typeof key) !== -1) {
+        key = [key];
+    }
+    let currentValue = obj;
+    for (let k of key) {
+        currentValue = currentValue[k];
+        if (currentValue === undefined) {
+            return defaultValue;
+        }
+    }
+    return currentValue;
 }
-
-
-module.exports = global_functions
---
+++ test/getattr.spec.js
@@ -0,0 +1,51 @@
+import { getattr } from '../src/main.js'
+import { expect } from 'chai'
+
+describe('global', function () {
+    describe('#getattr', function () {
+        it('when key exists should return the value', function () {
+            let obj = { 'key': 1 }
+            expect(getattr(obj, 'key')).to.equal(1)
+        })
+        it('when key exists should return the value', function () {
+            let obj = [1]
+            expect(getattr(obj, 0)).to.equal(1)
+        })
+        it('when key is an array with a valid key should return the value', function () {
+            let obj = { 'key': 1 }
+            expect(getattr(obj, ['key'])).to.equal(1)
+        })
+        it('when key is an array with a valid key should return the value', function () {
+            let obj = [1]
+            expect(getattr(obj, [0])).to.equal(1)
+        })
+        it('when key is an array with recursive valid keys should return the value', function () {
+            let obj = { 'key_1': { 'key_2': 1 } }
+            expect(getattr(obj, ['key_1', 'key_2'])).to.equal(1)
+        })
+        it('when key is an array with recursive valid keys should return the value', function () {
+            let obj = [[0, 0], [1, 0]]
+            expect(getattr(obj, [1, 0])).to.equal(1)
+        })
+        it('when key is an array with recursive valid keys should return the value', function () {
+            let obj = { 'key_1': [1] }
+            expect(getattr(obj, ['key_1', 0])).to.equal(1)
+        })
+        it('when key dosn\'t exists should return the default value', function () {
+            let obj = { 'key': 1 }
+            expect(getattr(obj, 'wrong_key', 'default')).to.equal('default')
+        })
+        it('when obj is null should return the default value', function () {
+            let obj = null;
+            expect(getattr(obj, 'key', 'default')).to.equal('default')
+        })
+        it('when obj is undefined should return the default value', function () {
+            let obj = undefined;
+            expect(getattr(obj, 'key', 'default')).to.equal('default')
+        })
+        it('when obj isn\'t a object or a array should throw an error', function () {
+            let obj = 1;
+            expect(() => getattr(obj, 'key')).to.throw()
+        })
+    })
+})
