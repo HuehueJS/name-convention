19703d2cf673a5173aca56f0a7c5b5b32d292b91
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-05-03 16:48:58 -0300

    splitters test

--- test/splitters.spec.js
+++ test/splitters.spec.js
@@ -4,7 +4,7 @@ import { Splitters } from '../src/name-convention/splitter/splitters';
 
 describe('Splitter',function(){
 
-    describe('#CameCase',function(){
+    describe('#CamelCase',function(){
         it('should split a camel case name',function(){
             let name = 'aComplexName';
             let expected = ['a','Complex','Name'];
@@ -15,6 +15,48 @@ describe('Splitter',function(){
             let name = 'aComplexNameWithNumbers123';
             let expected = ['a','Complex','Name','With','Numbers','123'];
             let result = Splitters.CamelCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+    })
+    describe('#SnakeCase',function(){
+        it('should split a snake case name',function(){
+            let name = 'a_Complex_name';
+            let expected = ['a','Complex','name'];
+            let result = Splitters.SnakeCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+        it('should split a camel case name',function(){
+            let name = 'a_Complex_Name_With_Numbers_123';
+            let expected = ['a','Complex','Name','With','Numbers','123'];
+            let result = Splitters.SnakeCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+    })
+    describe('#KebabCase',function(){
+        it('should split a kebab case name',function(){
+            let name = 'a-Complex-name';
+            let expected = ['a','Complex','name'];
+            let result = Splitters.KebabCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+        it('should split a kebab case name',function(){
+            let name = 'a-Complex-Name-With-Numbers-123';
+            let expected = ['a','Complex','Name','With','Numbers','123'];
+            let result = Splitters.KebabCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+    })
+    describe('#AnyCase',function(){
+        it('should split a mix case name',function(){
+            let name = 'aComplex_name';
+            let expected = ['a','Complex','name'];
+            let result = Splitters.AnyCase.split(name);
+            expect(result).to.deep.equal(expected);
+        })
+        it('should split a mix case name',function(){
+            let name = 'aComplex-Name_With-Numbers123';
+            let expected = ['a','Complex','Name','With','Numbers','123'];
+            let result = Splitters.AnyCase.split(name);
             expect(result).to.deep.equal(expected);
         })
     })
