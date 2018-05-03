7ab3c95d54304ae4eeb9d12d7d9bd640e2d1fd06
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-05-03 09:27:22 -0300

    refactoring

--- src/name-convention/gluers.js
+++ src/name-convention/gluers.js
@@ -1,23 +1,21 @@
 import { DefaultGluer } from './default-gluer.js';
 import { upperCase, lowerCase, upperCaseFirst, lowerCaseFirst } from '../string.js';
 
-
-export const LowerCamelCaseGluer = new DefaultGluer(
-    '',
-    upperCaseFirst,
-    lowerCaseFirst
-);
-
-export const UpperCamelCaseGluer = new DefaultGluer(
-    '',
-    upperCaseFirst
-);
-
-export const LowerSnakeCase = new DefaultGluer(
-    '_'
-);
-
-export const UpperSnakeCase = new DefaultGluer(
-    '_',
-    upperCase
-);
+export const Gluers = {
+    LowerCamelCase: new DefaultGluer(
+        '',
+        upperCaseFirst,
+        lowerCaseFirst
+    ),
+    UpperCamelCase: new DefaultGluer(
+        '',
+        upperCaseFirst
+    ),
+    LowerSnakeCase: new DefaultGluer(
+        '_'
+    ),
+    UpperSnakeCase: new DefaultGluer(
+        '_',
+        upperCase
+    )
+}
--- test/gluers.spec.js
+++ test/gluers.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import *  as Gluers from '../src/name-convention/gluers.js';
+import { Gluers } from '../src/name-convention/gluers.js';
 
 
 describe('Gluers', function () {
@@ -7,14 +7,14 @@ describe('Gluers', function () {
         it('should return a lower camel case name', function () {
             let pieces = ['a','complex','name'];
             let expected = 'aComplexName';
-            let result = Gluers.LowerCamelCaseGluer.glue(pieces);
+            let result = Gluers.LowerCamelCase.glue(pieces);
             console.log(result);
             expect(result).to.equal(expected);
         })
         it('should return a lower camel case name', function () {
             let pieces = ['a','complex','name','with','numbers','123'];
             let expected = 'aComplexNameWithNumbers123';
-            let result = Gluers.LowerCamelCaseGluer.glue(pieces);
+            let result = Gluers.LowerCamelCase.glue(pieces);
             console.log(result);
             expect(result).to.equal(expected);
         })
@@ -23,14 +23,14 @@ describe('Gluers', function () {
         it('should return a upper camel case name', function () {
             let pieces = ['a','complex','name'];
             let expected = 'AComplexName';
-            let result = Gluers.UpperCamelCaseGluer.glue(pieces);
+            let result = Gluers.UpperCamelCase.glue(pieces);
             console.log(result);
             expect(result).to.equal(expected);
         })
         it('should return a upper camel case name', function () {
             let pieces = ['a','complex','name','with','numbers','123'];
             let expected = 'AComplexNameWithNumbers123';
-            let result = Gluers.UpperCamelCaseGluer.glue(pieces);
+            let result = Gluers.UpperCamelCase.glue(pieces);
             console.log(result);
             expect(result).to.equal(expected);
         })
