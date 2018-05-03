e63de452540e40efc6969a452d406eadb9d40c50
Éttore Leandro Tognoli <ettore.tognoli@ciag.org.br>
2018-05-03 11:02:21 -0300

    rm log

--- test/gluers.spec.js
+++ test/gluers.spec.js
@@ -8,14 +8,12 @@ describe('Gluers', function () {
             let pieces = ['a','complex','name'];
             let expected = 'aComplexName';
             let result = Gluers.LowerCamelCase.glue(pieces);
-            console.log(result);
             expect(result).to.equal(expected);
         })
         it('should return a lower camel case name', function () {
             let pieces = ['a','complex','name','with','numbers','123'];
             let expected = 'aComplexNameWithNumbers123';
             let result = Gluers.LowerCamelCase.glue(pieces);
-            console.log(result);
             expect(result).to.equal(expected);
         })
     })
@@ -24,14 +22,12 @@ describe('Gluers', function () {
             let pieces = ['a','complex','name'];
             let expected = 'AComplexName';
             let result = Gluers.UpperCamelCase.glue(pieces);
-            console.log(result);
             expect(result).to.equal(expected);
         })
         it('should return a upper camel case name', function () {
             let pieces = ['a','complex','name','with','numbers','123'];
             let expected = 'AComplexNameWithNumbers123';
             let result = Gluers.UpperCamelCase.glue(pieces);
-            console.log(result);
             expect(result).to.equal(expected);
         })
     })
