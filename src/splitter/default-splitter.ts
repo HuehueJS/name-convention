df86c1348e13abf6bc0da3dc4a327583cad9f245
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-10-24 22:47:52 -0300

    splitter

--- src/splitter/default-splitter.ts
+++ src/splitter/default-splitter.ts
@@ -1,14 +1,19 @@
 import { isNullOrUndefined } from '@huehuejs/common-lang';
 
-export class DefaultSplitter {
+
+export interface Splitter {
+    split(name: string): Array<string>;
+}
+
+export class DefaultSplitter implements Splitter {
 
     constructor(
-        protected preparerRegex,
-        protected splitterRegex = new RegExp(' +', 'g'),
-        protected replacePattern = '$1 $2'
+        protected preparerRegex: RegExp = null,
+        protected splitterRegex: RegExp = new RegExp(' +', 'g'),
+        protected replacePattern: string = '$1 $2'
     ) { }
 
-    split(name) {
+    split(name: string): Array<string> {
         if (isNullOrUndefined(name)) {
             throw Error("Name cannot be undefined!");
         }
